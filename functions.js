a302ad96eba535f00727aa543d67a04f303268ce
Gabriel Alexis F. Gonzales <gabriel@iamusic.ga>
2023-06-29 01:05:58 +0800

    sample

---
+++ functions.js
@@ -0,0 +1,7 @@
+function hi() {
+    console.log("Hello nigga");
+}
+function hi2(op1, op2) {
+    console.log(op1 + op2);
+}
+export { hi, hi2 };
---
+++ index.js
@@ -0,0 +1,4 @@
+import { hi, hi2 } from "./functions.js";
+console.log("Hello World!");
+hi();
+hi2(1, 1);
