5a55223f466714ed5cd8f064330156517b6e2e41
RealiamusicGabriel <gfgonzales@donbosco.edu.ph>
2023-06-29 23:07:27 +0800

    Tailwind and Webpack Dependency

---
+++ dist/bundle.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log("Hello World!"),console.log("Hello nigga"),console.log(2)})();
--- src/index.js
+++ src/index.js
@@ -2,4 +2,3 @@ import { hi, hi2 } from "./functions.js";
 console.log("Hello World!");
 hi();
 hi2(1, 1);
-//This is a change 3
