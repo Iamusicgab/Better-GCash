9c618d78d2da50de9fbb6994d6c0f123227b2847
RealiamusicGabriel <gfgonzales@donbosco.edu.ph>
2023-07-01 01:31:39 +0800

    Firebase Partial Authentication Configuration

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
-import { hi, hi2 } from "./functions.js";
-console.log("Hello World!");
-hi();
-hi2(1, 1);
+//Configuration and connection to firebase
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCuJJLIhMJ1erv6vRVFLbpYuiDpYc7JVnU",
+  authDomain: "better-gcash.firebaseapp.com",
+  databaseURL: "https://better-gcash-default-rtdb.firebaseio.com",
+  projectId: "better-gcash",
+  storageBucket: "better-gcash.appspot.com",
+  messagingSenderId: "293907249950",
+  appId: "1:293907249950:web:5aa7b7a0bfba14e0e3a70f",
+  measurementId: "G-YD5KN6HJRB"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const auth = getAuth(app);
