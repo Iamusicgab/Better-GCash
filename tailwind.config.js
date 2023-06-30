2f2ed9ec87505c9d8c596ec7ff5d5ca1e19069fa
RealiamusicGabriel <gfgonzales@donbosco.edu.ph>
2023-07-01 00:59:57 +0800

    Dark Mode fix and Pictures

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,34 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        "primary": "#007cff",
+        "secondary": "#6fbaf7",   
+        "accent": "#002cb8",   
+        "neutral": "#6b7280",
+        "base-100": "#f3f4f6",
+        "info": "#3abff8",
+        "success": "#36d399",
+        "warning": "#fbbd23",
+        "error": "#f87272"
+        },
+        mythemedark: {
+        "primary": "#007cff",
+        "secondary": "#6fbaf7",   
+        "accent": "#002cb8",   
+        "neutral": "#6b7280",
+        "base-100": "#111827",
+        "info": "#3abff8",
+        "success": "#36d399",
+        "warning": "#fbbd23",
+        "error": "#f87272"
+        }
+      }
+      ]
+    }
+    
 }
 
