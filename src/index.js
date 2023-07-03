9a7747239108cd89bc1bcacbd2b8053a226787ce
Iamusicgab <gfgonzales@donbosco.edu.ph>
2023-07-04 03:30:19 +0800

    Final Firebase Authentication Configuration 1

--- src/index.js
+++ src/index.js
@@ -1,24 +1,78 @@
-//Login Imports
-import { inputEmail, inputPassword, btnLogin } from "./ui";
-
 //Firebase Config
 //NOTE: The other Firebase API keys and config are in the firebasecf.js file
 import { app } from "./firebasecf";
 
-//Firebase Imports
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+//Login Imports
+import { inputEmail, inputPassword, btnLogin, loginSignup, logoutBtn, errorBlock, errorField } from "./ui";
 
-const auth = getAuth(app);
+//Firebase Imports
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut, AuthErrorCodes } from "firebase/auth";
+
+
 
 console.log(btnLogin);
 
-//Login Functions
+//AUTHENTICATION FUNCTIONS
+const auth = getAuth(app);
+
+//Monitor Authentication State
+const monitorAuthState = async () => {
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            console.log("User is logged in");
+            showLoggedIn();
+        } else {
+            console.log("User is logged out");
+            showSignedOut();
+        }
+    })
+}
+
+function showLoggedIn() {
+    balanceForm.classList.remove("hidden");
+    loginForm.classList.add("hidden");
+}
+function showSignedOut() {
+    balanceForm.classList.add("hidden");
+    loginForm.classList.remove("hidden");
+}
+
+monitorAuthState();
+
+//Login
 const loginEmailPassword = async () => {
     const loginEmail = inputEmail.value;
     const loginPassword = inputPassword.value;
 
-    const userCredential = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
-    console.log(userCredential.user);
+    try {
+        const userCredential = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
+        console.log(userCredential.user);
+    }
+    catch (error) {
+        console.log(error);
+        loginError(error);
+    }
+
 }
 
 btnLogin.addEventListener("click", loginEmailPassword);
+
+//Logout
+const logout = async () => {
+    await signOut(auth);
+}
+
+logoutBtn.addEventListener("click", logout);
+
+//Error
+export const loginError = (error) => {
+    errorBlock.classList.remove("hidden");
+    if (error.code == AuthErrorCodes.INVALID_PASSWORD) {
+        errorField.innerHTML = "Wrong Password";
+    } else if (error.code == AuthErrorCodes.USER_NOT_FOUND) {
+        errorField.innerHTML = "User Not Found";
+    } else {
+        console.log(error.code);
+        errorField.innerHTML = error.code;
+    }
+}
--- src/ui.js
+++ src/ui.js
@@ -1,3 +1,7 @@
 export const inputEmail = document.querySelector("#inputEmail");
 export const inputPassword = document.querySelector("#inputPassword");
 export const btnLogin = document.querySelector("#btnLogin");
+export const loginSignup = document.querySelector("#loginSignup");
+export const logoutBtn = document.querySelector("#logoutBtn");
+export const errorBlock = document.querySelector("#errorBlock");
+export const errorField = document.querySelector("#errorField");
