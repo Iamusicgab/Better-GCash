0a56520dfdae112217052da28011debbb18b6bdf
Iamusicgab <gfgonzales@donbosco.edu.ph>
2023-07-07 12:30:39 +0800

    Transaction History Placeholder

--- dist/signup.bundle.js
+++ dist/signup.bundle.js
@@ -34035,14 +34035,14 @@ __webpack_require__.r(__webpack_exports__);
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCuJJLIhMJ1erv6vRVFLbpYuiDpYc7JVnU",
-  authDomain: "better-gcash.firebaseapp.com",
-  databaseURL: "https://better-gcash-default-rtdb.firebaseio.com",
-  projectId: "better-gcash",
-  storageBucket: "better-gcash.appspot.com",
-  messagingSenderId: "293907249950",
-  appId: "1:293907249950:web:5aa7b7a0bfba14e0e3a70f",
-  measurementId: "G-YD5KN6HJRB"
+	apiKey: "AIzaSyCuJJLIhMJ1erv6vRVFLbpYuiDpYc7JVnU",
+	authDomain: "better-gcash.firebaseapp.com",
+	databaseURL: "https://better-gcash-default-rtdb.firebaseio.com",
+	projectId: "better-gcash",
+	storageBucket: "better-gcash.appspot.com",
+	messagingSenderId: "293907249950",
+	appId: "1:293907249950:web:5aa7b7a0bfba14e0e3a70f",
+	measurementId: "G-YD5KN6HJRB"
 };
 
 const app = (0,firebase_app__WEBPACK_IMPORTED_MODULE_0__.initializeApp)(firebaseConfig);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,41 +2,38 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
-  },
-  plugins: [
-
-    require("daisyui"),
-  
-  ],
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-        "primary": "#007cff",
-        "secondary": "#6fbaf7",   
-        "accent": "#002cb8",   
-        "neutral": "#6b7280",
-        "base-100": "#f3f4f6",
-        "info": "#3abff8",
-        "success": "#36d399",
-        "warning": "#fbbd23",
-        "error": "#f87272"
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+      extend: {},
+    },
+    plugins: [require("daisyui")],
+    daisyui: {
+      themes: [
+        {
+          mytheme: {
+            primary: "#007cff",
+            secondary: "#6fbaf7",
+            accent: "#002cb8",
+            neutral: "#6b7280",
+            "base-100": "#f3f4f6",
+            info: "#3abff8",
+            success: "#36d399",
+            warning: "#fbbd23",
+            error: "#f87272",
+          },
+          mythemedark: {
+            primary: "#007cff",
+            secondary: "#6fbaf7",
+            accent: "#002cb8",
+            neutral: "#6b7280",
+            "base-100": "#111827",
+            info: "#3abff8",
+            success: "#36d399",
+            warning: "#fbbd23",
+            error: "#f87272",
+          },
         },
-        mythemedark: {
-        "primary": "#007cff",
-        "secondary": "#6fbaf7",   
-        "accent": "#002cb8",   
-        "neutral": "#6b7280",
-        "base-100": "#111827",
-        "info": "#3abff8",
-        "success": "#36d399",
-        "warning": "#fbbd23",
-        "error": "#f87272"
-        }
-      }
-      ]
-    }
-    
-}
-
+      ],
+    },
+  },
+};
